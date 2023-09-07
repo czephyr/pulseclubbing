16e86e25184c81379da4f937dc09fe3e0be12fe4
czephyr <alfredo.funicello@studenti.unimi.it>
2023-09-07 22:23:55 +0200

    fix: updated gitignore

--- cronjob.py
+++ cronjob.py
@@ -6,7 +6,8 @@ import sqlite3
 
 
 if __name__ == '__main__':
-    # THIS NEEDS TO BE REWRITTEN TO RETURN A LIST NOT A DF
+    # THIS NEEDS TO BE REWRITTEN TO RETURN A LIST NOT A DF? Can return a df if too difficult
+    # just care about the event input of insert_event
     # df = pd.DataFrame()
     # df = fanfulla.scrape(df)
     # df = ra.scrape(df)
