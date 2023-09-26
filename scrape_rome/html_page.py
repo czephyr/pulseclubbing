23132b4a96dd02bb7d79f6e80981cdb6e270ccd1
czephyr <alfredo.funicello@studenti.unimi.it>
2023-09-26 20:45:59 +0200

    added is_clubbing to db

--- scrape_rome/html_page.py
+++ scrape_rome/html_page.py
@@ -29,7 +29,7 @@ def update_webpage(db_connection, file_to_write:str):
 
             # Add each link as a list item
             for event in sorted(events,key=lambda x: datetime.strptime(x[2], '%Y-%m-%d %H:%M:%S')):
-                id,name,date,artists,organizer,location,price,link,descr,is_valid = event
+                id,name,date,artists,organizer,location,price,link,descr,is_valid,is_clubbing = event
                 html_content += f'''
                         <li>
                             <a href="{link if link[:4] == 'http' else 'https://' + link}" target="_blank" db_id={id}>{organizer} || {name}</a>
