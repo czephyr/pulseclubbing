43d796da2a5a6ee64fe71d3a25807e16061906f0
czephyr <alfredo.funicello@studenti.unimi.it>
2024-02-04 17:48:37 +0100

    added that events are in rome

--- scrape_rome/html_page.py
+++ scrape_rome/html_page.py
@@ -360,7 +360,7 @@ def get_upper_part(is_next_month):
             <a class="title" href="/">
                 <h1></h1>
             </a>
-            <p class="telegram-link"><a href="https://t.me/quindistase" target="_blank">Join the Telegram group: <span class="underline-text" style="color: #dd8521;">@quindistase</span></a></p>
+            <p class="telegram-link">Selected clubbing events in Rome. <a href="https://t.me/quindistase" target="_blank">Join the Telegram group: <span class="underline-text" style="color: #dd8521;">@quindistase</span></a></p>
             <nav>
                 <p><a href="/">Home</a></p><p>""" + next_month_button +"""</oomp>
             </nav>
